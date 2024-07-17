32722109e80cfca3ca861ae533807909e105eb79
brendaahumada <bahumada@mail.austral.edu.ar>
2024-07-17 16:48:31 -0300

    sidebar with ChakraUI

--- common/components/SideBar.jsx
+++ common/components/SideBar.jsx
@@ -1,119 +1,166 @@
 'use client';
+import { useState } from 'react';
+import { Box, Flex, IconButton, Text } from "@chakra-ui/react";
+import { ArrowForwardIcon, ArrowBackIcon } from "@chakra-ui/icons";
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { usePathname } from 'next/navigation';
 
 const SideBar = () => {
-    const [collapsed, setCollapsed] = useState(true);
-    const router = useRouter();
-    const currentPath = usePathname();
+  const [collapsed, setCollapsed] = useState(true);
+  const router = useRouter();
+  const currentPath = router.pathname;
 
-
-const toggleSidebar = () => {
+  const toggleSidebar = () => {
     setCollapsed(!collapsed);
-};
+  };
 
-  // const handleMouseEnter = () => {
-  //   setCollapsed(false);
-  // };
+  const isActiveLink = (href) => currentPath === href;
 
-  // const handleMouseLeave = () => {
-  //   setCollapsed(true);
-  // };
-
-const isActiveLink = (href) => currentPath === href
-
-return (
-    <aside className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
-        <div className='sidebar_top'>
-        <Image
-        src={collapsed ? "/images/collapsedaustral.png" : "/images/image%20austral.png"}
-        width={collapsed ? 75.52 : 200}
-        height={collapsed ? 90.54 : 75}
-        className='Sidebar_logo'
-        alt='logo'
-        priority
-        />
-        </div>
-        <ul className='sidebar_list'>
-        {/* <li className='sidebar_item'>
-        <Link href="/" className={`sidebar_link ${isActiveLink('/') ? 'active' : ''}`}>
-            <span className={`sidebar_text ${collapsed ? 'hidden' : ''}`}>Home</span>
-        </Link>
-        </li> */}
-        <li className='sidebar_item'>
-        <Link href="/Administradores" className={`sidebar_link ${isActiveLink('/Administradores') ? 'active' : ''}`}>
-            <div>
+  return (
+    <Flex>
+      <Flex
+        direction="column"
+        align="center"
+        bg="#14218D"
+        height="100vh"
+        width={collapsed ? "6.5rem" : "17rem"}
+        transition="width 0.5s ease-in-out"
+        justifyContent="space-between"
+        position="fixed"
+        left="0"
+        top="0"
+        zIndex="1"
+      >
+        <Box w="100%">
+          <Box display="flex" alignItems="center" justifyContent="center" py="1rem" mb="1rem">
             <Image
-                src='/icons/Administradores-icon.svg'
-                width={30}
-                height={30}
-                className='sidebar_icon'
-                alt=''
-                priority={true}
+              src={collapsed ? "/images/collapsedaustral.png" : "/images/image%20austral.png"}
+              width={collapsed ? 75.52 : 200}
+              height={collapsed ? 90.54 : 75}
+              alt='logo'
+              priority
             />
-            </div>
-            <span className={`sidebar_text ${collapsed ? 'hidden' : ''}`}>Administradores</span>
-        </Link>
-        </li>
-        <li className='sidebar_item'>
-        <Link href="/Tutores" className={`sidebar_link ${isActiveLink('/Tutores') ? 'active' : ''}`}>
-            <div>
-            <Image
-                src='/icons/tutors-icon.svg'
-                alt=''
-                width={30}
-                height={30}
-                className='sidebar_icon'
-            />
-            </div>
-            <span className={`sidebar_text ${collapsed ? 'hidden' : ''}`}>Tutores</span>
-        </Link>
-        </li>
-        <li className='sidebar_item'>
-        <Link href="/Alumnos" className={`sidebar_link ${isActiveLink('/Alumnos') ? 'active' : ''}`}>
-            <div>
-            <Image
-                src='/icons/student-icon.svg'
-                alt='Alumnos'
-                className='sidebar_icon'
-                width={30}
-                height={30}
-            />
-            </div>
-            <span className={`sidebar_text ${collapsed ? 'hidden' : ''}`}>Alumnos</span>
-        </Link>
-        </li>
-        <li className='sidebar_item'>
-        <Link href="/Reuniones" className={`sidebar_link ${isActiveLink('/Reuniones') ? 'active' : ''}`}>
-            <div>
-            <Image
-                src='/icons/Reuniones-icon.svg'
-                alt='Alumnos'
-                width={30}
-                height={30}
-                className='sidebar_icon'
-            />
-            </div>
-            <span className={`sidebar_text ${collapsed ? 'hidden' : ''}`}>Reuniones</span>
-        </Link>
-        </li>
-    </ul>
-    {/* <div className={'container_sidebar_button_back'}>
-        <button type='button' onClick={() => router.back()} className='sidebar_button_back'>
-        ←
-        </button>
-      </div> */}
-    <div className="container_toggle_sidebar_button">
-    <button type='button' onClick={toggleSidebar} className='toggle_sidebar_button'>
-        {collapsed ? '  ⭢' : '←'}
-    </button>
-    </div>
-    
-    </aside>
-);
+          </Box>
+          <Flex direction="column" as="ul" listStyleType="none" p={0}>
+            <Box as="li" mb="1rem">
+              <Link href="/Administradores" passHref>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  p="0.8rem 1rem"
+                  bg={isActiveLink('/Administradores') ? "#318AE4" : "#14218D"}
+                  color={isActiveLink('/Administradores') ? "white" : "#fff3e9"}
+                  borderRadius="10px"
+                  transition="background-color 0.1s ease-in-out"
+                  _hover={{ bg: "#318AE4", color: "white" }}
+                  justifyContent={collapsed ? "center" : "flex-start"}
+                  textDecoration="none"
+                >
+                  <Image
+                    src='/icons/Administradores-icon.svg'
+                    width={30}
+                    height={30}
+                    alt=''
+                    priority={true}
+                  />
+                  {!collapsed && <Text ml="0.5rem">Administradores</Text>}
+                </Box>
+              </Link>
+            </Box>
+            <Box as="li" mb="1rem">
+              <Link href="/Tutores" passHref>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  p="0.8rem 1rem"
+                  bg={isActiveLink('/Tutores') ? "#318AE4" : "#14218D"}
+                  color={isActiveLink('/Tutores') ? "white" : "#fff3e9"}
+                  borderRadius="10px"
+                  transition="background-color 0.1s ease-in-out"
+                  _hover={{ bg: "#318AE4", color: "white" }}
+                  justifyContent={collapsed ? "center" : "flex-start"}
+                  textDecoration="none"
+                >
+                  <Image
+                    src='/icons/tutors-icon.svg'
+                    width={30}
+                    height={30}
+                    alt=''
+                    priority={true}
+                  />
+                  {!collapsed && <Text ml="0.5rem">Tutores</Text>}
+                </Box>
+              </Link>
+            </Box>
+            <Box as="li" mb="1rem">
+              <Link href="/Alumnos" passHref>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  p="0.8rem 1rem"
+                  bg={isActiveLink('/Alumnos') ? "#318AE4" : "#14218D"}
+                  color={isActiveLink('/Alumnos') ? "white" : "#fff3e9"}
+                  borderRadius="10px"
+                  transition="background-color 0.1s ease-in-out"
+                  _hover={{ bg: "#318AE4", color: "white" }}
+                  justifyContent={collapsed ? "center" : "flex-start"}
+                  textDecoration="none"
+                >
+                  <Image
+                    src='/icons/student-icon.svg'
+                    width={30}
+                    height={30}
+                    alt=''
+                    priority={true}
+                  />
+                  {!collapsed && <Text ml="0.5rem">Alumnos</Text>}
+                </Box>
+              </Link>
+            </Box>
+            <Box as="li" mb="1rem">
+              <Link href="/Reuniones" passHref>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  p="0.8rem 1rem"
+                  bg={isActiveLink('/Reuniones') ? "#318AE4" : "#14218D"}
+                  color={isActiveLink('/Reuniones') ? "white" : "#fff3e9"}
+                  borderRadius="10px"
+                  transition="background-color 0.1s ease-in-out"
+                  _hover={{ bg: "#318AE4", color: "white" }}
+                  justifyContent={collapsed ? "center" : "flex-start"}
+                  textDecoration="none"
+                >
+                  <Image
+                    src='/icons/Reuniones-icon.svg'
+                    width={30}
+                    height={30}
+                    alt=''
+                    priority={true}
+                  />
+                  {!collapsed && <Text ml="0.5rem">Reuniones</Text>}
+                </Box>
+              </Link>
+            </Box>
+          </Flex>
+        </Box>
+        <Box className="container_toggle_sidebar_button" mb="1rem">
+          <IconButton
+            colorScheme="blue"
+            color="white"
+            bg="#318AE4"
+            icon={collapsed ? <ArrowForwardIcon /> : <ArrowBackIcon />}
+            onClick={toggleSidebar}
+            aria-label="Toggle SideBar"
+            borderRadius="50%"
+          />
+        </Box>
+      </Flex>
+      <Box ml={collapsed ? "6.5rem" : "17rem"} width="100%" p="4">
+      </Box>
+    </Flex>
+  );
 };
 
 export default SideBar;
