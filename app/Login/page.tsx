c04bef2455fd278ebeab5792b92336cbd0f09abd
brendaahumada <bahumada@mail.austral.edu.ar>
2024-08-13 17:41:04 -0300

    fix errors in LoginPage

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -2,15 +2,17 @@
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { Flex, Stack, Box, Image, FormControl, Link, Input, Button, Text, FormHelperText, Container } from '@chakra-ui/react';
+import { Stack, Box, Image, FormControl, Link, Input, Button, Text, FormHelperText, Container, InputGroup, InputRightElement } from '@chakra-ui/react';
 
 const Login = () => {
   const [email, setEmail] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
+  const handleClick = () => setShowPassword(!showPassword);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -45,86 +47,91 @@ const Login = () => {
     <Container
       width="100vw"
       h="100vh"
-      backgroundImage={'/images/image_158.png'}
+      backgroundImage={'/images/LoginBackground.png'}
       backgroundSize="cover"
       backgroundPosition="center"
       backgroundRepeat="no-repeat"
+      display="flex"
       justifyContent="center"
       alignItems="center"
     >
-      <Stack
-        flexDirection="column"
-        mb="2"
-        justifyContent="center"
+      <Box
+        backgroundColor='#FCF5F9'
+        p='1rem'
+        borderRadius="10px"
+        shadow="md"
+        w={{ base: "90%", sm: "70%", md: "50%", lg: "40%" }}
       >
-        <Box>
-          <form onSubmit={handleSubmit}>
-            <Stack
-              spacing={4}
-              p='1rem'
-              backgroundColor='#FCF5F9'
-              borderRadius="10px"
-              height="320px"
-              w="538px"
-              alignItems="center"
-            >
-              <Image
-                src='/images/Image-Login-Photoroom.png'
-                alt='Image-login'
-                width="450px"
-                h="112px"
-                objectFit='contain'
+        <form onSubmit={handleSubmit}>
+          <Stack
+            spacing={4}
+            p='1rem'
+            borderRadius="10px"
+            alignItems="center"
+          >
+            <Image
+              src='/images/LoginFormImage.png'
+              alt='Image-login'
+              width="450px"
+              h="112px"
+              objectFit='contain'
+            />
+            {error && <Text color="red.500" textAlign="center">{error}</Text>}
+            
+            <FormControl>
+              <Input
+                borderRadius="3px"
+                border="none"
+                outline="none"
+                h="42px"
+                w='350px'
+                type='email'
+                placeholder='Email'
+                backgroundColor="#D9D9D9"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                paddingLeft="1.5rem"
               />
-              {error && <Text color="red.500">{error}</Text>}
-              <FormControl>
+            </FormControl>
+            
+            <FormControl>
                 <Input
                   borderRadius="3px"
                   border="none"
                   outline="none"
-                  h="35px"
-                  w='292px'
-                  type='email'
-                  placeholder='Email'
+                  h="42px"
+                  w='350px'
                   backgroundColor="#D9D9D9"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  paddingLeft="1.5rem"
-                />
-              </FormControl>
-              <FormControl>
-                <Input
-                  borderRadius="3px"
-                  border="none"
-                  outline="none"
-                  h="35px"
-                  w='292px'
-                  backgroundColor="#D4CBCB"
-                  type='password'
+                  type={showPassword ? 'text' : 'password'}
                   placeholder='Contraseña'
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   paddingLeft="1.5rem"
                 />
-                <FormHelperText textAlign="center">
-                  <Link>Si olvidaste tu contraseña, recupérala aquí</Link>
-                </FormHelperText>
-              </FormControl>
-              <Button
-                borderRadius="5px"
-                type="submit"
-                backgroundColor="#172187"
-                color="white"
-                width="357px"
-                height="42px"
-                mt={4}
-                isLoading={isLoading}
-              >
-                Iniciar Sesión
-              </Button>
-            </Stack>
-          </form>
-        </Box>
-      </Stack>
+            </FormControl>
+            
+            <Button
+              borderRadius="5px"
+              type="submit"
+              backgroundColor="#172187"
+              color="white"
+              width="100%"
+              maxW='300px'
+              height="42px"
+              mt={4}
+              isLoading={isLoading}
+            >
+              Iniciar Sesión
+            </Button>
+            
+            <FormControl>
+              <FormHelperText textAlign="center" mt={4}>
+                <Link>Si olvidaste tu contraseña, recupérala aquí</Link>
+              </FormHelperText>
+            </FormControl>
+          </Stack>
+        </form>
+      </Box>
     </Container>
   );
 }
