9b2f19d7b0bcdb8e18659e36d7e1106219b5fc5c
technicolo <franfrare19@gmail.com>
2025-08-22 17:28:18 -0300

    creado profile en minusculas

---
+++ app/profile/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { Flex } from "@chakra-ui/react";
+import ProfileComponent from './components/profile-component';
+
+const Profile = () => {
+  return (
+    <Flex
+      height="100vh"
+      bg="paleGray"
+      flexDirection="column"
+    >
+      <Flex justifyContent="center" alignItems="center" flex="1" mx="auto">
+        <ProfileComponent/>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Profile;
